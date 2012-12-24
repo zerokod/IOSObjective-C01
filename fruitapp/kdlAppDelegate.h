0dea0e9be73ec2318caaccca3845462aac243647
Roberta Marino <geeklab@MacBook-Pro-di-Roberta.local>
2012-12-24 15:43:46 +0100

    Initial Commit

---
+++ fruitapp/kdlAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  kdlAppDelegate.h
+//  fruitapp
+//
+//  Created by Roberta Marino on 24/12/12.
+//  Copyright (c) 2012 kodlounge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface kdlAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ fruitapp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  fruitapp
+//
+//  Created by Roberta Marino on 24/12/12.
+//  Copyright (c) 2012 kodlounge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "kdlAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([kdlAppDelegate class]));
+    }
+}
